ef7e672a8baba73bf3e9f9a992a170129ee05e0e
嗝屁 <1010017806@qq.com>
2019-08-28 13:53:43 +0800

    cc

--- logic_server/config/config.js
+++ logic_server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   helpEmail:"1010017806@qq.com",
   homePage:"",
   praiceLink:"",
-  payUrl:"",
-  payToken:"",
-  appID:"",
+  payUrl:"http://127.0.0.1:3000/order",
+  payToken:"NDORJpzm3x",
+  appID:"e8Vkw7qrX",
 }
