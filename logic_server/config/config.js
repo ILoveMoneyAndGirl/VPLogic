930256836b61cab2e49b1d70cd9e5fb1b9c5e30b
嗝屁 <1010017806@qq.com>
2019-09-09 14:28:27 +0800

    ccc

--- logic_server/config/config.js
+++ logic_server/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   mallurl:"dddd",
   helpQQ:"1010017806",
   helpEmail:"fishdivetechnology@gmail.com",
-  homePage:"https://github.com/fishdivetechnology/home",
+  homePage:"https://github.com/fishdivetechnology/home/wiki",
   praiceLink:"",
   payUrl:"http://pay.liguaika.xyz:3000/order",
   payToken:"q02PLH5Q",
