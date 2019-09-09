42dbe54c7297cdcf7dbe405db8d3cb51c2455340
嗝屁 <1010017806@qq.com>
2019-09-09 09:04:10 +0800

    ccc

--- logic_server/config/config.js
+++ logic_server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   helpEmail:"1010017806@qq.com",
   homePage:"",
   praiceLink:"",
-  payUrl:"http://139.162.40.184:3000/order",
-  payToken:"NDORJpzm3x",
-  appID:"e8Vkw7qrX",
+  payUrl:"http://pay.liguaika.xyz:3000/order",
+  payToken:"q02PLH5Q",
+  appID:"Owy2e3E",
 }
