05053a0b0ab8717da42edd962f4b17d889be03fe
嗝屁 <1010017806@qq.com>
2019-09-30 16:13:22 +0800

    cc

--- logic_server/config/config.js
+++ logic_server/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   helpQQ:"请访问https://github.com/fishdivetechnology/StormVPN，该页面有联系QQ",
   helpEmail:"fishdivetechnology@gmail.com",
   homePage:"https://github.com/fishdivetechnology/StormVPN",
-  chromeLink:"https://chrome.google.com/webstore/detail/gdgekinnjookhmchkemmnejcnfcaehhc/publish-delayed?hl=zh-CN&authuser=1",
+  chromeLink:"https://chrome.google.com/webstore/detail/stormvpn/gdgekinnjookhmchkemmnejcnfcaehhc?hl=zh-CN&authuser=1",
   praiceLink:"",
   payUrl:"http://pay.liguaika.xyz:3000/order",
   payToken:"q02PLH5Q",
