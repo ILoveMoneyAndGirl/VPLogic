39a6fb32b84a6d2a5341728c586c6387c0fe47ba
嗝屁 <1010017806@qq.com>
2019-10-10 08:54:42 +0800

    cc

--- logic_server/controller/host.js
+++ logic_server/controller/host.js
@@ -42,11 +42,11 @@ class Host {
           lastList[i]["address"]=list[i]["address"]
           lastList[i]["id"]=list[i]["id"]
           lastList[i]["status"]=list[i]["status"]
-          lastList[i]["info"]=""
+          lastList[i]["info"]=list[i]["info"]
             for (var j = 0; j < list.length; j++) {
                 if(j==i)
                     continue
-                lastList[i]["info"]=lastList[i]["info"]+list[i]["info"]+";"+list[j]["info"]
+                lastList[i]["info"]=lastList[i]["info"]+";"+list[j]["info"]
             }
         }
         console.log("_____nnnnnnnnnnnnnnnnnnnn____>")
