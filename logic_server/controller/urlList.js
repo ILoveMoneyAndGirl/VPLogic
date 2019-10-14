7de607347874cf057993210f28ffc89c436f0f4f
嗝屁 <1010017806@qq.com>
2019-10-14 11:46:49 +0800

    cc

--- logic_server/controller/urlList.js
+++ logic_server/controller/urlList.js
@@ -10,10 +10,14 @@ async  function _getURLByUser(name){
         t[urls[i].id]=urls[i].url
       }
 
-      let u1= await URLListModel.find({userName:config.commonUser})
-      for (var i = 0; i < u1.length; i++) {
-        t[u1[i].id]=u1[i].url
+      if(config.commonUser!=name)
+      {
+          let u1= await URLListModel.find({userName:config.commonUser})
+          for (var i = 0; i < u1.length; i++) {
+            t[u1[i].id]=u1[i].url
+          }
       }
+
 
       return t
 }
